eb2eba9244b10a6b66238b618533d7aaa40cb141
Ahmed Usama <aomn040@gmail.com>
2022-09-01 01:11:41 +0200

    my website

--- public/js/game.js
+++ public/js/game.js
@@ -1,1 +1,13 @@
-window.onload=function(){let loader=document.querySelector("#loader");loader.style.opacity="0",loader.style.zIndex="-1";let navbar=document.querySelector("#navbar");fetch("/layouts/navbar.html").then(a=>a.text()).then(data=>{navbar.innerHTML=data;let parser=new DOMParser,doc=parser.parseFromString(data,"text/html");eval(doc.querySelector("script").textContent)})}
+window.onload = function () {
+  let loader = document.querySelector("#loader");
+  (loader.style.opacity = "0"), (loader.style.zIndex = "-1");
+  let navbar = document.querySelector("#navbar");
+  fetch("./layouts/navbar.html")
+    .then((res) => res.text())
+    .then((data) => {
+      navbar.innerHTML = data;
+      let parser = new DOMParser(),
+        doc = parser.parseFromString(data, "text/html");
+      eval(doc.querySelector("script").textContent);
+    });
+};
