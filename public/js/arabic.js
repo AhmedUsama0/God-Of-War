e16c195676ceda814f5ea7f5604160cf89436b1b
Ahmed Usama <aomn040@gmail.com>
2022-09-01 21:37:14 +0200

    mywebsite

---
+++ public/js/arabic.js
@@ -0,0 +1,14 @@
+window.onload = function () {
+    let loader = document.querySelector("#loader");
+    (loader.style.opacity = "0"), (loader.style.zIndex = "-1");
+    let navbar = document.querySelector("#navbar");
+    fetch("../layouts/arabicNavbar.html")
+      .then((res) => res.text())
+      .then((data) => {
+        navbar.innerHTML = data;
+        let parser = new DOMParser(),
+          doc = parser.parseFromString(data, "text/html");
+        eval(doc.querySelector("script").textContent);
+      });
+  };
+  
