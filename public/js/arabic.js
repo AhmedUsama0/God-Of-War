40cdfbee1ba9a449d851e52a413d59c38fbc3e4a
Ahmed Usama <aomn040@gmail.com>
2022-09-06 15:12:54 +0200

    better code

--- public/js/arabic.js
+++ public/js/arabic.js
@@ -1,14 +1,5 @@
-window.onload = function () {
-    let loader = document.querySelector("#loader");
-    (loader.style.opacity = "0"), (loader.style.zIndex = "-1");
-    let navbar = document.querySelector("#navbar");
-    fetch("../layouts/arabicNavbar.html")
-      .then((res) => res.text())
-      .then((data) => {
-        navbar.innerHTML = data;
-        let parser = new DOMParser(),
-          doc = parser.parseFromString(data, "text/html");
-        eval(doc.querySelector("script").textContent);
-      });
-  };
-  
+import { getNavBar, finishLoading } from "./game.js";
+window.onload = () => {
+  finishLoading();
+  getNavBar("../layouts/arabicNavbar.html");
+};
--- public/js/game.js
+++ public/js/game.js
@@ -1,13 +1,19 @@
-window.onload = function () {
-  let loader = document.querySelector("#loader");
-  (loader.style.opacity = "0"), (loader.style.zIndex = "-1");
-  let navbar = document.querySelector("#navbar");
-  fetch("layouts/navbar.html")
-    .then((res) => res.text())
-    .then((data) => {
-      navbar.innerHTML = data;
-      let parser = new DOMParser(),
-        doc = parser.parseFromString(data, "text/html");
-      eval(doc.querySelector("script").textContent);
-    });
+window.onload = () => {
+  finishLoading();
+  getNavBar("layouts/navbar.html");
 };
+
+const getNavBar = async (path) => {
+  const navigation = await fetch(path).then((res) => res.text());
+  navbar.innerHTML = navigation;
+  const parser = new DOMParser(),
+    doc = parser.parseFromString(navigation, "text/html");
+  new Function(doc.querySelector("script").textContent)();
+};
+
+const finishLoading = () => {
+  loader.style.opacity = "0";
+  loader.style.zIndex = "-1";
+};
+
+export { getNavBar, finishLoading };
